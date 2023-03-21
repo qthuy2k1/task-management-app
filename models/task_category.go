e1a027713b23d2f76b9ae5cd9cb4b633f3b54b85
qthuy2k1 <qthuy2609@gmail.com>
2023-03-21 17:29:24 +0700

    create api for task categories

--- models/task_category.go
+++ models/task_category.go
@@ -1,10 +1,30 @@
 package models
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type TaskCategory struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
-type TaskCategories struct {
+type TaskCategoryList struct {
 	TaskCategories []TaskCategory `json:"task_categories"`
+}
+
+func (t *TaskCategory) Bind(r *http.Request) error {
+	if t.Name == "" {
+		return fmt.Errorf("name is a required field")
+	}
+	return nil
+}
+
+func (*TaskCategoryList) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (*TaskCategory) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
