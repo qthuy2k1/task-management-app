602bf1df04dd60a8a26aef25b494bbfeb9384597
qthuy2k1 <qthuy2609@gmail.com>
2023-04-21 11:02:50 +0700

    rename

---
+++ internal/controllers/task_controller.go
@@ -0,0 +1,211 @@
+package controllers
+
+import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	models "github.com/qthuy2k1/task-management-app/internal/models/gen"
+	"github.com/qthuy2k1/task-management-app/internal/repositories"
+	"github.com/volatiletech/null/v8"
+)
+
+type TaskController struct {
+	TaskRepository *repositories.TaskRepository
+}
+
+func NewTaskController(taskRepository *repositories.TaskRepository) *TaskController {
+	return &TaskController{TaskRepository: taskRepository}
+}
+
+func (c *TaskController) GetAllTasks(ctx context.Context, pageNumber int, pageSize int, sortField string, sortOrder string) (models.TaskSlice, error) {
+	tasks, err := c.TaskRepository.GetAllTasks(ctx, pageNumber, pageSize, sortField, sortOrder)
+	if err != nil {
+		return tasks, err
+	}
+	return tasks, nil
+}
+
+func (c *TaskController) AddTask(task *models.Task, ctx context.Context) error {
+	err := c.TaskRepository.AddTask(task, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *TaskController) GetTaskByID(taskID int, ctx context.Context) (*models.Task, error) {
+	task, err := c.TaskRepository.GetTaskByID(taskID, ctx)
+	if err != nil {
+		return task, err
+	}
+	return task, nil
+}
+
+func (c *TaskController) DeleteTask(taskID int, ctx context.Context) error {
+	err := c.TaskRepository.DeleteTask(taskID, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *TaskController) UpdateTask(taskID int, taskData models.Task, ctx context.Context, isManager bool) (*models.Task, error) {
+	task, err := c.TaskRepository.GetTaskByID(taskID, ctx)
+	if err != nil {
+		return task, err
+	}
+
+	task.Name = taskData.Name
+	task.Description = taskData.Description
+	task.StartDate = taskData.StartDate
+	task.EndDate = taskData.EndDate
+	task.Status = taskData.Status
+	if task.Status.String == "Lock" && !isManager {
+		return task, errors.New("you are not the manager, cannot lock this task")
+	}
+	task.AuthorID = taskData.AuthorID
+	task.UpdatedAt = time.Now()
+	task.TaskCategoryID = taskData.TaskCategoryID
+
+	taskUpdated, err := c.TaskRepository.UpdateTask(task, ctx)
+	if err != nil {
+		return taskUpdated, err
+	}
+	return taskUpdated, nil
+}
+
+func (c *TaskController) LockTask(taskID int, ctx context.Context) error {
+	err := c.TaskRepository.LockTask(taskID, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *TaskController) UnLockTask(taskID int, ctx context.Context) error {
+	err := c.TaskRepository.UnLockTask(taskID, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Import tasks data from a CSV file
+func (c *TaskController) ImportTaskDataFromCSV(path string) ([]models.Task, error) {
+	// Create a slice to store the task data
+	taskList := []models.Task{}
+
+	// Open the CSV file
+	file, err := os.Open(path)
+	if err != nil {
+		return taskList, err
+	}
+	defer file.Close()
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
+	// Trim leading spaces around fields
+	reader.TrimLeadingSpace = true
+
+	// Read all the records from the CSV file
+	records, err := reader.ReadAll()
+	if err != nil {
+		return taskList, err
+	}
+
+	// Layout for time.Time field in struct
+	const layoutDate = "2006-01-02T15:04:05Z"
+
+	// Loop through the records and create a new Task struct for each record
+	for i, record := range records {
+		// Skip the first index, which is the header name
+		if i == 0 {
+			continue
+		}
+		data := strings.Split(record[0], ",")
+		// remove unexpected characters
+		re := regexp.MustCompile("[^a-zA-Z0-9 ]+")
+
+		// regex for Name
+		name := re.ReplaceAllString(data[0], "")
+
+		// regex for description
+		description := re.ReplaceAllString(data[1], "")
+
+		// convert start date from string to time.Time
+		startDate, err := time.Parse(layoutDate, data[2])
+		if err != nil {
+			return taskList, err
+		}
+
+		// convert end date from string to time.Time
+		endDate, err := time.Parse(layoutDate, data[3])
+		if err != nil {
+			return taskList, err
+		}
+
+		// regex for task status
+		status := re.ReplaceAllString(data[4], "")
+
+		// convert authorID from string to int
+		authorID, err := strconv.Atoi(data[5])
+		if err != nil {
+			return taskList, err
+		}
+
+		// convert createdAt from string to time.Time
+		createdAt, err := time.Parse(layoutDate, data[6])
+		if err != nil {
+			return taskList, err
+		}
+
+		// convert updatedAt from string to time.Time
+		updatedAt, err := time.Parse(layoutDate, data[7])
+		if err != nil {
+			return taskList, err
+		}
+
+		// convert taskCategoryID from string to int
+		taskCategoryID, err := strconv.Atoi(data[8])
+		if err != nil {
+			return taskList, err
+		}
+
+		task := models.Task{
+			Name:           name,
+			Description:    description,
+			StartDate:      startDate,
+			EndDate:        endDate,
+			Status:         null.StringFrom(status),
+			AuthorID:       authorID,
+			CreatedAt:      createdAt,
+			UpdatedAt:      updatedAt,
+			TaskCategoryID: taskCategoryID,
+		}
+		taskList = append(taskList, task)
+	}
+
+	return taskList, nil
+}
+
+func (c *TaskController) GetTaskCategoryOfTask(taskID int, ctx context.Context) (*models.TaskCategory, error) {
+	taskCategory, err := c.TaskRepository.GetTaskCategoryOfTask(taskID, ctx)
+	if err != nil {
+		return taskCategory, err
+	}
+	return taskCategory, err
+}
+
+func (c *TaskController) GetTasksByName(name string, ctx context.Context) (models.TaskSlice, error) {
+	tasks, err := c.TaskRepository.GetTasksByName(name, ctx)
+	if err != nil {
+		return tasks, err
+	}
+	return tasks, nil
+}
---
+++ internal/controllers/user_controller.go
@@ -0,0 +1,181 @@
+package controllers
+
+import (
+	"context"
+	"errors"
+	"html"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	models "github.com/qthuy2k1/task-management-app/internal/models/gen"
+	"github.com/qthuy2k1/task-management-app/internal/repositories"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserController struct {
+	UserRepository *repositories.UserRepository
+}
+
+func NewUserController(userRepository *repositories.UserRepository) *UserController {
+	return &UserController{UserRepository: userRepository}
+}
+
+func (c *UserController) GetAllUsers(ctx context.Context) (models.UserSlice, error) {
+	users, err := c.UserRepository.GetAllUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+func (c *UserController) AddUser(user *models.User, ctx context.Context) error {
+	// Sanitize and hash password
+	password := html.EscapeString(strings.TrimSpace(user.Password))
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	hashedPasswordStr := string(hashedPassword)
+	user.Password = hashedPasswordStr
+	if err != nil {
+		return err
+	}
+	err = c.UserRepository.AddUser(user, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (c *UserController) GetUserByID(userID int, ctx context.Context) (*models.User, error) {
+	user, err := c.UserRepository.GetUserByID(userID, ctx)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+func (c *UserController) GetUserByEmail(userEmail string, ctx context.Context) (*models.User, error) {
+	user, err := c.UserRepository.GetUserByEmail(userEmail, ctx)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (c *UserController) DeleteUser(userID int, ctx context.Context, r *http.Request, tokenAuth *jwtauth.JWTAuth, token jwt.Token) error {
+	rowsAff, err := c.UserRepository.DeleteUser(userID, ctx)
+	if err != nil {
+		return err
+	}
+	if rowsAff == 0 {
+		return errors.New("no row affected")
+	}
+	return nil
+}
+
+func (c *UserController) UpdateUser(userID int, userData models.User, ctx context.Context) (*models.User, error) {
+	user, err := c.UserRepository.GetUserByID(userID, ctx)
+	if err != nil {
+		return user, err
+	}
+
+	user.Name = userData.Name
+	user.Email = userData.Email
+	userUpdated, err := c.UserRepository.UpdateUser(user, ctx)
+	if err != nil {
+		return user, err
+	}
+	return userUpdated, nil
+}
+
+func (c *UserController) UpdateRole(userID int, role string, ctx context.Context, r *http.Request, tokenAuth *jwtauth.JWTAuth) (*models.User, error) {
+	user, err := c.UserRepository.GetUserByID(userID, ctx)
+	if err != nil {
+		return user, err
+	}
+
+	user.Role = role
+	userUpdated, err := c.UserRepository.UpdateUser(user, ctx)
+	if err != nil {
+		return user, err
+	}
+	return userUpdated, nil
+}
+
+// Checks if a user is a manager
+func (c *UserController) IsManager(ctx context.Context, r *http.Request, tokenAuth *jwtauth.JWTAuth) error {
+	token, err := tokenAuth.Decode(jwtauth.TokenFromCookie(r))
+	if err != nil {
+		return err
+	}
+
+	email, _ := token.Get("email")
+
+	// Convert email from interface{} to string
+	emailStr, ok := email.(string)
+	if !ok {
+		return errors.New("cannot convert email from interface to string")
+	}
+
+	isManager, err := c.UserRepository.IsManager(ctx, emailStr)
+
+	if err != nil {
+		return err
+	}
+	if !isManager {
+		return errors.New("you are not the manager")
+	}
+
+	return nil
+}
+
+func (c *UserController) CompareEmailAndPassword(email, password string, ctx context.Context) (bool, error) {
+	users, err := c.UserRepository.GetAllUsers(ctx)
+	if err != nil {
+		return false, err
+	}
+	for _, x := range users {
+		if x.Email == email {
+			if err = bcrypt.CompareHashAndPassword([]byte(x.Password), []byte(password)); err == nil {
+				return true, nil
+			} else {
+				return false, errors.New("your password is wrong")
+			}
+		}
+	}
+	return false, errors.New("your email is wrong")
+}
+
+func (c *UserController) ChangeUserPassword(oldPassword, newPassword, email string, ctx context.Context, r *http.Request, tokenAuth *jwtauth.JWTAuth) error {
+	newPassword = html.EscapeString(strings.TrimSpace(newPassword))
+	oldPassword = html.EscapeString(strings.TrimSpace(oldPassword))
+
+	user, err := c.UserRepository.GetUserByEmail(email, ctx)
+	if err != nil {
+		return err
+	}
+
+	// Compare password hashed in db to the old password got from the form value
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(oldPassword)); err != nil {
+		return errors.New("incorrect old password")
+	}
+
+	// Hash new password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), 14)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(hashedPassword)
+
+	_, err = c.UserRepository.UpdateUser(user, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *UserController) GetUsersManager(ctx context.Context) (models.UserSlice, error) {
+	users, err := c.UserRepository.GetUsersManager(ctx)
+	if err != nil {
+		return users, err
+	}
+	return users, err
+}
